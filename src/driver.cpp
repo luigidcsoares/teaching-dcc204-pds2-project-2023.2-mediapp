ff8318a28eb3eda02085d34fe7c4dcbf9b61d62d
Luigi D. C. Soares <luigidcsoares@gmail.com>
2023-10-31 09:52:05 -0300

    Removendo cin.ignore

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,19 +1,13 @@
 #include "ui/login_menu.hpp"
-
 #include <iostream>
-#include <limits>
 
 namespace ui = mediapp::ui;
 int main() {
-  long int const max_input =
-    std::numeric_limits<std::streamsize>::max();
-  
   ui::LoginMenu login;
   login.render();
   
   unsigned option;
   std::cin >> option;
-  std::cin.ignore(max_input, '\n');
 
   ui::Menu *menu = login.next(option);
   return 0;
